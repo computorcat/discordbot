c60b6e1b3f2815a1c1bf3248bcc52839654b313d
computorcat <hannahtisback@gmail.com>
2023-07-31 21:04:08 +0100

    improvments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import discord
-from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-TOKEN = "MTEyMTU0MzkxNTg4Mzk5MTE1MA.GJk_jW.LO6zzusEViNNls5oD1ov0GFHqRHsJaRpRF3i6I" 
+# Load environment variables from .env file
+load_dotenv()
+# markov feature
+# !search <query> will search for a query on google and return the first result 
 # This example requires the 'message_content' intent.
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+prefix = "evilbot "
 client = discord.Client(intents=intents)
 
 @client.event
@@ -17,7 +21,28 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    # color role giver bot. user can !color <color> to get a color role which will be given to them. the role will be named after the user, and will be changed if the user changes their name or color.
-    if message.content.startswith('evilbot say hello'):
+    
+    if message.content.startswith(prefix + 'hello'):
         await message.channel.send('hello')
-client.run(TOKEN)
+        
+    if message.content.startswith(prefix + 'say'):
+        # put everything after evilbot say into a variable
+        say = message.content.split(' ')[2:]
+        say = ' '.join(say)
+        await message.channel.send(say)
+        print(say)
+
+    if client.user.mentioned_in(message):
+        await message.channel.send('hey!')
+        
+    if message.content.startswith(prefix + 'help'):
+        await message.channel.send('\n\n prefix is ``evilbot``. \n\n ``evilbot help`` - shows this message \n\n``evilbot say <message>`` - makes the bot say whatever is in <message> \n\n ``evilbot hello`` - evilbot will say hi! \n\n if you ping evilbot they will say "hey!"')
+        
+    if message.content.startswith(prefix + 'calc'):
+        # calculate the math in the message
+        await message.channel.send("still workin' on it!")
+        
+    if "evilbot" in message.content:
+        await message.add_reaction('☣️')
+        
+client.run(os.getenv('TOKEN'))
