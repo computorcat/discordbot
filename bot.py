08d08204af4d17742043d8a6ff754d5fa9a4dcf9
computorcat <hannahtisback@gmail.com>
2023-07-31 13:11:53 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,3 @@
+# color role giver bot. user can !color <color> to get a color role which will be given to them. the role will be named after the user, and will be changed if the user changes their name or color.
+import discord
+from discord.ext import commands
