f0254fbd0472092bbf61c96a86e2f464a50f6365
computorcat <hannahtisback@gmail.com>
2023-08-01 13:26:00 +0100

    cool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 import os
 from dotenv import load_dotenv
-
 # you need a .env file i fear
 load_dotenv()
 
@@ -10,9 +9,13 @@ load_dotenv()
 # !search <query> will search for a query on google and return the first result 
 # evilbot should not react to command messages!
 # umm the ah!!! calculator code and what ever else i add that is complex in a seperate file please
+# moderation commands <--  remember to give evilbot the perms to do this
+# a levelling system?? 
+# i guess all those make it count as an "general purpose" bot
+# also - reverse image search and music player < -- will get to this when i read more discord documentation
 intents = discord.Intents.default()
 intents.message_content = True
-prefix = "evilbot "
+prefix = os.getenv('PREFIX')
 client = discord.Client(intents=intents)
 
 @client.event
@@ -31,7 +34,10 @@ async def on_message(message):
         # put everything after evilbot say into a variable
         say = message.content.split(' ')[2:] # everyhing after "evilbot say" (well should be)
         say = ' '.join(say)
-        await message.channel.send(say)
+        if say == "":
+            await message.channel.send("you didn't give me anything to say!")
+        else:
+            await message.channel.send(say)
         #print(say) <-- debug
 
     if client.user.mentioned_in(message):
@@ -39,12 +45,20 @@ async def on_message(message):
         
     if message.content.startswith(prefix + 'help'):
         # ok thiis sucks but it does the job i think
-        await message.channel.send('\n\n prefix is ``evilbot``. \n\n ``evilbot help`` - shows this message \n\n``evilbot say <message>`` - makes the bot say whatever is in <message> \n\n ``evilbot hello`` - evilbot will say hi! \n\n if you ping evilbot they will say "hey!"')
+        await message.channel.send('\n\n prefix is ``'+prefix+'``. \n\n ``'+prefix+'help`` - shows this message \n\n``'+prefix+'say <message>`` - makes the bot say whatever is in <message> \n\n ``'+prefix+'hello`` - evilbot will say hi! \n\n if you ping evilbot they will say "hey!"')
         
     if message.content.startswith(prefix + 'calc'):
         # calculate the math in the message
         await message.channel.send("still workin' on it!")
-    if "evilbot" in message.content:
-        await message.add_reaction('☣️')
+        math = message.content.split(' ')[2:]
+        math = ' '.join(math)
+        if math == "":
+            await message.channel.send("you didn't give me anything to calculate!")
+        else:
+            await message.channel.send("you're asking me to calculate " + math + ", right?")
+            
+    # you can comment this out if you want the people say its chaotic!
+    #if "evilbot" in message.content:
+    #    await message.add_reaction('☣️')
         
 client.run(os.getenv('TOKEN'))
